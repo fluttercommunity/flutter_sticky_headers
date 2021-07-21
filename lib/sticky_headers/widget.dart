b3ab5ef11a1de4b01a4ba390fb48ecb71fd42ce5
Cosminnv <48117967+Cosminnv@users.noreply.github.com>
2021-07-21 17:26:19 +0200

    Update widget.dart

--- lib/sticky_headers/widget.dart
+++ lib/sticky_headers/widget.dart
@@ -1,4 +1,4 @@
-/ Copyright 2018 Simon Lightfoot. All rights reserved.
+// Copyright 2018 Simon Lightfoot. All rights reserved.
 // Use of this source code is governed by a the MIT license that can be
 // found in the LICENSE file.
 
